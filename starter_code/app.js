51e87fba2a4b19685f0f18654464c7ceac0507be
Matthew Larrubia <mlarrubia@columbushs.com>
2019-07-03 13:51:55 -0400

    First Commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,9 +6,19 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+
+// -------------   Express   --------------------------------
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+//  --------------  Partials   --------------------------------
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+// ---------------   Pages   -----------------------------------
 
 
 
@@ -16,6 +26,36 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers.name);
+    
+    
+  
+    
+    res.render('beers', {bunchaBeers: beers})
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+
+});
+
+app.get('/random-beer', (req, res, next) => {
+  
+  punkAPI.getRandom()
+  .then(beers => {   
+
+    res.render('randomBeer', {random:  beers[0]});
+    
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
 
 
 app.listen(3000);
